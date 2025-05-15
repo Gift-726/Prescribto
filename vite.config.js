c997ff5c11764ce0e2e4097fcd80bf11ffbb7430
Gift-726 <125131320+Gift-726@users.noreply.github.com>
2025-05-15 12:40:10 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from 'tailwindcss'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/Prescribto",
   css: {
     postcss: {
       plugins: [tailwindcss()],
